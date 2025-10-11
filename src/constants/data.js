a5cca3de4afad5e2cd913e130928737e19aa9fbd
RANVIJAY KUMAR <116416700+ranvijay-kumar4@users.noreply.github.com>
2025-10-11 16:08:40 +0530

    Update data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -216,7 +216,7 @@ export const personalInfo = {
 
 // Education Data
 export const education = {
-  degree: "Bachelor of Technology in Information Technology",
+  degree: "Bachelor of Technology - IT",
   institution: "Jabalpur Engineering College, Jabalpur",
   duration: "2022 - 2026",
 };
